3c025a7a1aa2f1b1bfc60b1aa26d4b12d82978c1
Shiv Vyas <68937501+shivvyasstudy@users.noreply.github.com>
2023-03-06 16:51:54 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions ={
+    headerStyle:{backgroundColor: "#FCFDF2" },
+    headerTitleStyle: {color: "#222222"},
+    headerTintColor: {color: "#222222"},
+
+    
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+       <NavigationContainer>
+            <Stack.Navigator screenOptions = {globalScreenOptions}>
+            <Stack.Screen name = "Login" component = {LoginScreen}/>
+            <Stack.Screen name = "Register" component = {RegisterScreen}/>
+             <Stack.Screen name = "Home" component = {HomeScreen}/> 
+
+
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFDF2',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ firebase.js
@@ -0,0 +1,30 @@
+// import * as firebase from "firebase";
+// import "firebase/firestore";
+// import "firebase/auth";
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCBcyGJo6X1QYKGFSxwzIyYLgMpCyDStuM",
+//     authDomain: "ash-1-a36e0.firebaseapp.com",
+//     projectId: "ash-1-a36e0",
+//     storageBucket: "ash-1-a36e0.appspot.com",
+//     messagingSenderId: "403343809632",
+//     appId: "1:403343809632:web:46308e50b59f8f2e16f705"
+//   };
+  
+// let app;
+
+// if ( firebase.apps.length === 0){
+//   app = firebase.initializeApp(firebaseConfig);
+// }else{
+//   app = firebase.app(); // or else it will keep on initializing
+
+// }
+
+// const db = app.firestore();
+// const auth = firebase.auth();
+
+
+// export{ db, auth };
+
+
