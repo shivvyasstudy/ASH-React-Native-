15dd1e0a0de13e396940b9008269aa89e6d803b1
Shiv Vyas <68937501+shivvyasstudy@users.noreply.github.com>
2023-03-07 13:53:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import HomeScreen from './screens/HomeScreen';
-
-const Stack = createNativeStackNavigator();
-
-const globalScreenOptions ={
-    headerStyle:{backgroundColor: "#FCFDF2" },
-    headerTitleStyle: {color: "#222222"},
-    headerTintColor: {color: "#222222"},
-
-    
-}
-
+//screen 
+import Login from './screens/Login';
 
 export default function App() {
   return (
-       <NavigationContainer>
-            <Stack.Navigator screenOptions = {globalScreenOptions}>
-            <Stack.Screen name = "Login" component = {LoginScreen}/>
-            <Stack.Screen name = "Register" component = {RegisterScreen}/>
-             <Stack.Screen name = "Home" component = {HomeScreen}/> 
+      <Login/>
+      
 
-
-          </Stack.Navigator>
-      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FCFDF2',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
