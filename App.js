52d83db6cfe90da0e6e1c537503ca6215087e247
shivvyas <shivvyasstudy@gmail.com>
2023-03-26 15:07:55 +0530

    App Working Commit

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
+import AddChatScreen from './screens/AddChatScreen';
+import ChatScreen from './screens/ChatScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions ={
+    headerStyle:{backgroundColor: "#FCFDF2" },
+    headerTitleStyle: {color: "#222222"},
+    headerTintColor: {color: "#222222"},
+
+    
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+       <NavigationContainer>
+            <Stack.Navigator 
+            // initialRouteName='Chat'
+            // initialRouteName= "Home"
+            initialRouteName='demo'
+             screenOptions = {globalScreenOptions}>
+            <Stack.Screen name = "Login" component = {LoginScreen}/>
+            <Stack.Screen name = "Register" component = {RegisterScreen}/>
+            <Stack.Screen name = "Home" component = {HomeScreen}/> 
+            <Stack.Screen name = "AddChat" component = {AddChatScreen}/> 
+            <Stack.Screen name = "Chat" component = {ChatScreen}/> 
+            {/* <Stack.Screen name = 'demo' component ={demo}/> */}
+
+
+
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFDF2',
     alignItems: 'center',
     justifyContent: 'center',
   },
